2978c1871ac3be892ff1bf465b210179f230498b
Emerson-13 <gonzalesemerson079@gmail.com>
2026-06-29 21:51:10 +0800

    initial update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  const isDev = mode === 'development'
 
   return {
     plugins: [react()],
-    base: env.VITE_BASE_PATH || '/',
+    base: env.VITE_BASE_PATH || (isDev ? '/' : '/my-portfolio/'),
   }
 })
